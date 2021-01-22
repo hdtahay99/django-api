b419f35873c0466237d78733bd5ec493743da934
Heansell Tahay <hdtahay@gmail.com>
2021-01-22 16:39:04 -0600

    added offer's endpoints

--- subasta/offers/urls.py
+++ subasta/offers/urls.py
@@ -6,9 +6,14 @@ from django.urls import path, include
 
 
 # Views
-from subasta.offers.views import create_offer
+from subasta.offers.views import create_offer, list_offer_product, list_offers_product
+# from .views import offers as offers_views
+
 
 
 urlpatterns = [
     path('offers/product/', create_offer),
+    path('offers/products/list/', list_offer_product),
+    path('offers/product/list/', list_offers_product),
+
 ]
--- subasta/offers/views/offers.py
+++ subasta/offers/views/offers.py
@@ -3,6 +3,7 @@
 # Django REST Framework
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
 # Models
@@ -14,17 +15,55 @@ from subasta.products.models import Product
 from subasta.offers.serializers import OfferModelSerializer, CreateOfferSerializer
 
 
+
+
 @api_view(['POST'])
 def create_offer(request):
     """Create product offer."""
 
-
     product = Product.objects.get(id=request.data.get('product'))
-    print(type(product))
-    nuevo = Offer.objects.create(
-        user=request.user,
-        product=product,
-        price_offer=request.data.get('price_offer')
-    )
+    if hasattr(request.user, 'is_client'):
+        if request.user.is_client == True and product.is_auctioned == False and product.is_active == True:
+            nuevo = Offer.objects.create(
+                user=request.user,
+                product=product,
+                price_offer=request.data.get('price_offer')
+            )
 
-    return Response(OfferModelSerializer(nuevo).data)
+            return Response(OfferModelSerializer(nuevo).data)
+        else:
+            return Response({'error' : 'Please, comunicated with de administrator, the user is not permited or the product already auctioned'}, status=status.HTTP_423_LOCKED)
+    else:
+        return Response({'error' : 'Please, comunicated with de administrator, the user is not permited'}, status=status.HTTP_403_FORBIDDEN)
+
+
+
+@api_view(['GET'])
+def list_offer_product(request):
+    """List the products's offer"""
+    
+    if hasattr(request.user, 'is_client'):
+        if request.user.is_admin == True:
+            products_offereds = Offer.objects.select_related('product').values('user','product', 'price_offer')
+            return Response(products_offereds)
+        else:
+            return Response({'error' : 'Please, comunicated with de administrator, the user is not permited'}, status=status.HTTP_403_FORBIDDEN)
+    else:
+        return Response({'error' : 'Please, comunicated with de administrator, the user is not permited'}, status=status.HTTP_403_FORBIDDEN)
+
+
+
+@api_view(['GET'])
+def list_offers_product(request):
+    """List the the product's offers"""
+
+    if hasattr(request.user, 'is_client'):
+        if request.user.is_client == True:
+            products_offereds = Offer.objects.filter(product=request.data.get('product')).select_related('product').values('user','product', 'price_offer')
+            return Response(products_offereds)
+        else:
+            return Response({'error' : 'Please, comunicated with de administrator, the user is not permited'}, status=status.HTTP_403_FORBIDDEN)    
+    else:
+        return Response({'error' : 'Please, comunicated with de administrator, the user is not permited'}, status=status.HTTP_403_FORBIDDEN)
+
+
