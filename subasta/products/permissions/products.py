3fa50b433b77c2a7787c3beff5cc0825714f9724
Heansell Tahay <hdtahay@gmail.com>
2021-01-22 17:30:25 -0600

    set the config for only admin users can do crud in products

---
+++ subasta/products/permissions/products.py
@@ -0,0 +1,23 @@
+
+
+"""Products permissions classes."""
+
+# Django REST Framework
+from rest_framework.permissions import BasePermission
+
+
+# Models
+from subasta.users.models import User
+
+class IsUserAdmin(BasePermission):
+    """Allow access only the admin users."""
+
+
+    def has_permission(self, request, view):
+        """Check that the user is admin."""
+
+        if view.action in ['list', 'create',' upadte', 'partial_update']:
+            return request.user.is_authenticated and request.user.is_admin
+        else:
+            return False
+
--- subasta/products/views/products.py
+++ subasta/products/views/products.py
@@ -1,8 +1,9 @@
 """Products views."""
 
 # Django REST Framework
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.permissions import IsAuthenticated
+from subasta.products.permissions.products import IsUserAdmin
 
 # Serializers
 from subasta.products.serializers import ProductModelSerializer
@@ -12,12 +13,27 @@ from subasta.products.serializers import ProductModelSerializer
 from subasta.products.models import Product
 
 
-class ProductViewSet(viewsets.ModelViewSet):
+class ProductViewSet(mixins.CreateModelMixin, 
+                    mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
     """Products view set."""
 
     queryset = Product.objects.filter(is_auctioned=False)
     serializer_class   = ProductModelSerializer
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsUserAdmin,)
+
+    
+
+    # def get_permissions(self):
+    #     """Assign permissions based on action."""
+
+    #     permissions = [IsAuthenticated]
+    #     if self.action in ['list', 'create',' upadte', 'partial_update']:
+    #         permissions.append()
+    #     return [permission() for permission in permissions]
+
 
 
     # def get_queryset(self):
